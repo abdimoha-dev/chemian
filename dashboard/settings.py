666c8120b59b75b1316097c54ec309561b253d8d
abdy254 <abdymohammed@gmail.com>
2020-10-18 14:30:07 +0300

    Update settings.py

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -19,7 +19,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'chemianhealth@gmail.com'
 EMAIL_HOST_PASSWORD = 'Bootyeater@69'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
